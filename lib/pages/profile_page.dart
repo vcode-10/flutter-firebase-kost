4648f28b95518f9e9a98610f7da5ab79b8a8a3a6
vcode-10 <vidibachrum@gmail.com>
2023-11-25 15:53:06 +0700

    fix profile page #1

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:projectkost/pages/auth_checker.dart';
 import 'package:projectkost/core/app_export.dart';
 import 'package:projectkost/core/utils/validation_functions.dart';
+import 'package:projectkost/pages/home_profile.dart';
 import 'package:projectkost/widgets/app_bar/appbar_leading_image.dart';
 import 'package:projectkost/widgets/app_bar/appbar_subtitle.dart';
 import 'package:projectkost/widgets/app_bar/custom_app_bar.dart';
@@ -26,90 +28,88 @@ class _ProfilePageState extends State<ProfilePage> {
   final TextEditingController _dateOfBirthController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _phoneNumberController = TextEditingController();
+  final user = FirebaseAuth.instance.currentUser;
+  final storageRef = FirebaseStorage.instance.ref();
+
+  String? earthRefUrl;
+
+  @override
+  void initState() {
+    super.initState();
+    _fullNameController.text = '';
+    _emailController.text = '';
+    _nameController.text = '';
+    _phoneNumberController.text = '';
+    _dateOfBirthController.text = '';
+    readData();
+  }
+
+  void readData() {
+    DatabaseReference usersRef =
+        FirebaseDatabase.instance.ref('users/${user?.uid}');
+
+    usersRef.onValue.listen((DatabaseEvent event) {
+      final data = event.snapshot.value;
+      print(data);
+      if (data != null && data is Map) {
+        // Set the URL of the image
+        setState(() {
+          _fullNameController.text = data['fullname'] ?? '';
+          _emailController.text = data['email'] ?? '';
+          _nameController.text = data['name'] ?? '';
+          _phoneNumberController.text = data['phoneNumber'] ?? '';
+          _dateOfBirthController.text = data['dateOfBirth'] ?? '';
+        });
+      } else {
+        print('No data found for the current user.');
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final user = FirebaseAuth.instance.currentUser;
-    final DatabaseReference _userRef =
-        FirebaseDatabase.instance.ref('users/${user!.uid}');
-    Stream<DataSnapshot> userStream =
-        _userRef.onValue.map((event) => event.snapshot);
-
     return AuthChecker(
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: _buildAppBar(),
-        body: Form(
-          key: _formKey,
-          child: StreamBuilder<DataSnapshot>(
-            stream: userStream,
-            builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
-              } else if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              } else if (!snapshot.hasData) {
-                return const Center(child: Text('No user data found'));
-              } else {
-                final userData = snapshot.data!.value;
-                if (userData is Map<String, dynamic>?) {
-                  String fullName = userData['fullName'] as String? ?? '';
-                  String name = userData['name'] as String? ?? '';
-                  String dateOfBirth = userData['dateOfBirth'] as String? ?? '';
-                  String email = userData['email'] as String? ?? '';
-                  String phoneNumber = userData['phoneNumber'] as String? ?? '';
-
-                  setState(() {
-                    _fullNameController.text = fullName;
-                    _nameController.text = name;
-                    _dateOfBirthController.text = dateOfBirth;
-                    _emailController.text = email;
-                    _phoneNumberController.text = phoneNumber;
-                  });
-                }
-
-                return Container(
-                  width: double.maxFinite,
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 24.h, vertical: 20.v),
-                  child: Column(children: [
-                    SizedBox(
-                        height: 160.adaptSize,
-                        width: 160.adaptSize,
-                        child:
-                            Stack(alignment: Alignment.bottomRight, children: [
-                          CustomImageView(
-                              imagePath: ImageConstant.imgEllipse160x160,
-                              height: 160.adaptSize,
-                              width: 160.adaptSize,
-                              radius: BorderRadius.circular(80.h),
-                              alignment: Alignment.center),
-                          CustomImageView(
-                              imagePath: ImageConstant.imgEdit,
-                              height: 40.adaptSize,
-                              width: 40.adaptSize,
-                              alignment: Alignment.bottomRight)
-                        ])),
-                    SizedBox(height: 24.v),
-                    _buildFullName(),
-                    SizedBox(height: 24.v),
-                    _buildName(),
-                    SizedBox(height: 24.v),
-                    _buildDateOfBirth(),
-                    SizedBox(height: 24.v),
-                    _buildEmail(),
-                    SizedBox(height: 24.v),
-                    _buildPhoneNumber(),
-                    SizedBox(height: 5.v)
-                  ]),
-                );
-              }
-            },
-          ),
+        child: Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: _buildAppBar(),
+      body: Form(
+        key: _formKey,
+        child: Container(
+          width: double.maxFinite,
+          padding: EdgeInsets.symmetric(horizontal: 24.h, vertical: 20.v),
+          child: Column(children: [
+            SizedBox(
+                height: 160.adaptSize,
+                width: 160.adaptSize,
+                child: Stack(alignment: Alignment.bottomRight, children: [
+                  CustomImageView(
+                      imagePath: ImageConstant.imgEllipse160x160,
+                      height: 160.adaptSize,
+                      width: 160.adaptSize,
+                      radius: BorderRadius.circular(80.h),
+                      alignment: Alignment.center),
+                  CustomImageView(
+                      imagePath: ImageConstant.imgEdit,
+                      height: 40.adaptSize,
+                      width: 40.adaptSize,
+                      alignment: Alignment.bottomRight)
+                ])),
+            SizedBox(height: 24.v),
+            _buildFullName(),
+            SizedBox(height: 24.v),
+            _buildName(),
+            SizedBox(height: 24.v),
+            _buildDateOfBirth(),
+            SizedBox(height: 24.v),
+            _buildEmail(),
+            SizedBox(height: 24.v),
+            _buildPhoneNumber(),
+            SizedBox(height: 5.v)
+          ]),
         ),
-        bottomNavigationBar: _buildContinue(),
       ),
-    );
+      bottomNavigationBar: _buildContinue(),
+    ));
   }
 
   /// Section Widget
@@ -120,7 +120,12 @@ class _ProfilePageState extends State<ProfilePage> {
             imagePath: ImageConstant.imgArrowLeft,
             margin: EdgeInsets.only(left: 24.h, top: 14.v, bottom: 13.v),
             onTap: () {
-              onTapArrowLeft();
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const HomeProfile(),
+                ),
+              );
             }),
         title: AppbarSubtitle(
             text: "Profile Saya".tr, margin: EdgeInsets.only(left: 16.h)));
@@ -161,19 +166,22 @@ class _ProfilePageState extends State<ProfilePage> {
   /// Section Widget
   Widget _buildDateOfBirth() {
     return CustomTextFormField(
-        controller: _dateOfBirthController,
-        hintText: "Tanggal Lahir".tr,
-        hintStyle: theme.textTheme.bodyMedium!,
-        suffix: Container(
-            margin: EdgeInsets.fromLTRB(30.h, 18.v, 20.h, 18.v),
-            child: CustomImageView(
-                imagePath: ImageConstant.imgIconlyCurvedCalendar,
-                height: 20.adaptSize,
-                width: 20.adaptSize)),
-        suffixConstraints: BoxConstraints(maxHeight: 56.v),
-        contentPadding: EdgeInsets.only(left: 20.h, top: 19.v, bottom: 19.v),
-        borderDecoration: TextFormFieldStyleHelper.fillGray,
-        fillColor: appTheme.gray50);
+      controller: _dateOfBirthController,
+      hintText: "Tanggal Lahir".tr,
+      hintStyle: theme.textTheme.bodyMedium!,
+      suffix: Container(
+        margin: EdgeInsets.fromLTRB(16.h, 12.v, 12.h, 12.v), // Adjust margin
+        child: CustomImageView(
+          imagePath: ImageConstant.imgIconlyCurvedCalendar,
+          height: 20.adaptSize,
+          width: 20.adaptSize,
+        ),
+      ),
+      suffixConstraints: BoxConstraints(maxHeight: 56.v),
+      contentPadding: EdgeInsets.only(left: 20.h, top: 19.v, bottom: 19.v),
+      borderDecoration: TextFormFieldStyleHelper.fillGray,
+      fillColor: appTheme.gray50,
+    );
   }
 
   /// Section Widget
@@ -205,24 +213,14 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget _buildPhoneNumber() {
     return CustomTextFormField(
         controller: _phoneNumberController,
-        hintText: "Whatsapp".tr,
+        hintText: "Nomor Handphone".tr,
         hintStyle: theme.textTheme.bodyMedium!,
-        textInputAction: TextInputAction.done,
-        prefix: Padding(
-            padding: EdgeInsets.fromLTRB(20.h, 19.v, 30.h, 19.v),
-            child: Row(mainAxisSize: MainAxisSize.min, children: [
-              CustomImageView(
-                  imagePath: ImageConstant.imageNotFound,
-                  height: 18.v,
-                  width: 24.h,
-                  margin: EdgeInsets.fromLTRB(20.h, 19.v, 30.h, 19.v)),
-              CustomImageView(
-                  imagePath: ImageConstant.imageNotFound,
-                  height: 16.adaptSize,
-                  width: 16.adaptSize)
-            ])),
-        prefixConstraints: BoxConstraints(maxHeight: 56.v),
-        contentPadding: EdgeInsets.only(top: 19.v, right: 30.h, bottom: 19.v),
+        validator: (value) {
+          if (!isText(value)) {
+            return "err_msg_please_enter_valid_text".tr;
+          }
+          return null;
+        },
         borderDecoration: TextFormFieldStyleHelper.fillGray,
         fillColor: appTheme.gray50);
   }
